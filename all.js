6ec791ad108aa3cf0838861cc958f3e108e0fae2
hzlzh <hzlzh.dev@gmail.com>
2011-09-28 08:23:39 +0000

    All.js fixed and fr_FR.po added.

--- all.js
+++ all.js
@@ -1,8 +1,8 @@
 // All the jQuery function that WordPress used
 // Add empty span tag at menu which have drop-down meun
-jQuery("ul.sub-menu:not(.sub-menu .sub-menu),ul.children:not(.children .children)").parent().append('<div class="menu-mark" ></div>');
+jQuery("ul.sub-menu:not(.sub-menu .sub-menu)").parent().append('<div class="menu-mark" ></div>');
 
-jQuery("ul.sub-menu ul.sub-menu:not(.sub-menu .sub-menu .sub-menu),ul.children ul.children:not(.children .children .children)").parent().append('<div class="menu-mark2" ></div>');
+jQuery("ul.sub-menu ul.sub-menu:not(.sub-menu .sub-menu .sub-menu)").parent().append('<div class="menu-mark2" ></div>');
 
 // Drop down menu slide function
 var mouseover_tid = [];
